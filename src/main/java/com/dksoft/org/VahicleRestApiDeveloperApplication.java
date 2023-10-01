aca689a84d2c6010056c83d682a010c93c729a01
Dinesh Kumar <DKDINESH.89KUMAR@GMAIL.COM>
2023-10-02 02:02:55 +0530

    Now this is the second commit for testing

--- src/main/java/com/dksoft/org/VahicleRestApiDeveloperApplication.java
+++ src/main/java/com/dksoft/org/VahicleRestApiDeveloperApplication.java
@@ -10,6 +10,7 @@ public class VahicleRestApiDeveloperApplication {
 		
 		SpringApplication.run(VahicleRestApiDeveloperApplication.class, args);
 		System.out.println("This is the best Or Super programming language in the world for me.");
+		System.out.println("Now we are adding the second line of the git testing code.");
 	}
 
 }
